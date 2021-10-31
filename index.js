02467b24b84fb25bce8ed18b3761e43b1d23f586
shannonssd <shannonssd@gmail.com>
2021-10-31 22:23:23 +0800

    Comfortable

--- index.js
+++ index.js
@@ -9,11 +9,29 @@ app.get('/recipe/:index', (req, res) => {
       console.log(err);
       return;
     }
-  const {index} = req.params;
-  if (!(index >= 0)){
-   res.status(404).send('Oi! Type properly! <br> Enter a number from 0');
-  }
-  res.send(content.recipes[index]);
+    const {index} = req.params;
+    if (!(index >= 0)){
+      res.status(404).send('Oi! Type properly! <br> Enter a number from 0');
+    }
+    res.send(content.recipes[index]);
+  })
+});
+
+app.get('/yield/:amount', (req, res) => {
+  read('data.json', (err, content) => {
+    if(err){
+      console.log(err);
+      return;
+    }
+    const { amount } = req.params;
+    const recipeArray = content.recipes;
+    let output = '';
+    for (let i = 0; i < recipeArray.length; i += 1){
+      if(recipeArray[i].yield === Number(amount)){
+        output += `Recipe: ${recipeArray[i].label} <br> Yield: ${recipeArray[i].yield} <br> Category: ${recipeArray[i].category} <br> <br>`;
+      }
+    }
+  res.send(output);
   })
 });
 
