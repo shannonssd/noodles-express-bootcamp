cc0a79bb2475284445f16d34a0fe1312a0111fbd
shannonssd <shannonssd@gmail.com>
2021-10-31 21:25:47 +0800

    Base

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js';
+
+const app = express();
+
+app.get('/recipe/:index', (req, res) => {
+  read('data.json', (err, content) => {
+    if(err){
+      console.log(err);
+      return;
+    }
+  const {index} = req.params;
+  if (!(index >= 0)){
+   res.status(404).send('Oi! Type properly! <br> Enter a number from 0');
+  }
+  res.send(content.recipes[index]);
+  })
+});
+
+app.listen(3004);
